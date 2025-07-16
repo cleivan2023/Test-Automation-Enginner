0a4985823a8d99ddd6003a4c06a93d1d41b753ee
Carol Leiva N <leivanaranjocarol@gmail.com>
2025-07-15 23:17:41 -0400

    Act. tiendaShopHup

---
+++ cypress/e2e/Tema4/Act.Tienda.ShopHub/Registro.ValidarCompra.spec.cy.js
@@ -0,0 +1,49 @@
+describe('Visit the Url to register an user', () => {
+    before(() => {
+        cy.visit('https://ecommerce-js-test.vercel.app/')
+    })
+
+    it('should register an user with valid params', () => {
+    cy.get('a[href="/register"]').click()
+    cy.fixture('usuarios.json').then((usuarios) => {
+      cy.get('#name').type(usuarios.usuarioExistente.name)
+      cy.get('#email').type(usuarios.usuarioExistente.email)
+      cy.get('#password').type(usuarios.usuarioExistente.password)
+      cy.get('#confirmPassword').type(usuarios.usuarioExistente.password)
+      cy.get('button[type="submit"]').click()
+      cy.get('span').contains('Hello').should('be.visible')
+      cy.get('#product_3 > div > div.p-4 > div.flex.items-center.justify-between > button').click()
+      cy.get('a[href="/cart"]').click()
+      cy.wait(3000)
+      cy.contains('button', 'Proceed to Checkout').click()
+      cy.on('window:alert', (text) => {
+        expect(text).include('Thank you for your purchase!')
+      })
+      cy.contains('button', 'Logout').click()
+      })
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+/* https://ecommerce-js-test.vercel.app/
+
+ACTIVIDAD:
+1. Registro e compra de mens Cotton Jacket. Validar comprar
+2. Login y compra de mens casual Slim fit. Validar comprar
+3. Búsqueda de SanDisk, detalle, Login para comprar. Validar comprar
+4. Búsqueda de WD 2TB, detalle, quantity 5, Login para comprar. Validar comprar
+5. Búsqueda mens, ordenar por el menor precio, clicar en el primer elemento, login para comprar. Validar comprar
+
+Email: admin@example.com
+Password: admin123      */
+
+
